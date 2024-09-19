035a79155ea42411141ba26cb69dd39d901a2240
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-09-19 19:36:00 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import { useState , React} from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css'
-import Home from './components/Home';
-import Recipes from './components/Recipes';
-import About from './components/About';
-import Search from './components/Search';
-import Navbar from './components/Navbar';
+import { useState, React } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Recipes from "./components/Recipes";
+import About from "./components/About";
+import Search from "./components/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Navbar/>
-        <div className="p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/search" element={<Search />} />
-
-          </Routes>
+      <Router>
+        <div className="min-h-screen  max-w-[1200px] m-auto">
+          <Navbar />
+          <div className="p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/recipes" element={<Recipes />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import userIcon from "../assets/User_icon.svg";
 import { CiSearch } from "react-icons/ci";
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <>
-      <nav className="p-4 flex justify-between items-center">
-        <div>
-          <h1 className="font-bold text-3xl">Recipe Calories</h1>
+      <nav className="p-4 flex space-y-4 lg:justify-between lg:items-center lg:flex-row flex-col ">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="font-bold text-3xl">Recipe Calories</h1>
+          </div>
+          <div
+            className="flex flex-col space-y-2 lg:hidden"
+            onClick={() => {
+              setIsNavOpen((prev) => !prev);
+            }}
+          >
+            <span className="block h-0.5 w-8 bg-gray-600 animate-pulse"></span>
+            <span className="block h-0.5 w-8 bg-gray-600 animate-pulse"></span>
+            <span className="block h-0.5 w-8 bg-gray-600 animate-pulse"></span>
+          </div>
         </div>
-        <ul className="flex space-x-10 font-semibold text-gray-500">
-          <li className="hover:text-black">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="hover:text-black">
-            <Link to="/recipes">Recipes</Link>
-          </li>
-          <li className="hover:text-black">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="hover:text-black">
-            <Link to="/search">Search</Link>
-          </li>
-        </ul>
-        <div className="flex">
+        <div className={!isNavOpen ? "flex " : "hidden"}>
+          <ul className="flex flex-col space-y-3 lg:space-y-0 w-full lg:space-x-10 lg:flex-row font-semibold text-gray-500 ">
+            <li className="hover:text-black">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="hover:text-black">
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li className="hover:text-black">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="hover:text-black">
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </div>
+        <div
+          className={
+            !isNavOpen
+              ? "flex xs:w-full justify-center items-center gap-5"
+              : "hidden"
+          }
+        >
           <div className="relative">
             <input
               type="text"
               name="search"
               placeholder="Search"
-              className="pl-8 pr-4 py-2 rounded-3xl"
+              className="pl-8 pr-4 py-2 border-2 border-red-400 rounded-3xl"
             />
             {/* Position the icon inside the input field */}
             <CiSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
-
           </div>
-          <button className="bg-lime-500 p-2 rounded-3xl"><img src={userIcon} alt="" srcset="" /></button>
+          <button className="bg-lime-500 p-2 rounded-3xl">
+            <img src={userIcon} alt="" srcset="" />
+          </button>
         </div>
       </nav>
     </>
