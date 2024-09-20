2cf35abf9a245b83ade7b363f15c60535ef3a85d
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-09-20 08:24:54 +0600

    ok a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <div
         style={{ backgroundImage: `url(${bgImg})` }}
-        className="banner lg:h-[480px] md:h-[600px] w-full relative flex justify-center items-center bg-cover bg-center"
+        className="banner lg:h-[480px] md:h-[600px] w-full relative flex justify-center items-center bg-cover bg-center p-8"
       >
         <div className="text-white  xl:w-3/5 flex flex-col justify-center px-4 gap-5 h-full">
           <h1 className="font-bold text-4xl leading-10">
