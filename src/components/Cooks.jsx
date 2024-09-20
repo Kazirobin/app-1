387d42a6e4a6a351f116c3a4cca8c64e6513db35
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-09-20 21:58:06 +0600

    ok

--- src/components/Cooks.jsx
+++ src/components/Cooks.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Cooks = ({cookList,id}) => {
     const {name,calories, preparing_time} = cookList
   return (
-    <div className='grid items-center grid-cols-[40px,2fr,1fr,1fr,1fr,1fr]'>
-        <p className='text-right'>{id+1}</p>
+    <div className='grid items-start grid-cols-[40px,2fr,1fr,1fr,1fr,1fr] my-4 py-2 border-b-2 border-gray-500'>
+        <p className='text-right place-self-end self-center font-bold p-2'>{id+1}</p>
         <p className='text-left pl-3'>{name}</p>
         <p>{preparing_time}</p>
         <p>{calories} calories</p>
