0604598eaea04436a8988b3be5e205252f5aa728
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-09-21 07:10:13 +0600

    ok

--- src/components/Cooks.jsx
+++ src/components/Cooks.jsx
@@ -3,21 +3,37 @@ import React from 'react'
 const Cooks = ({cookList,id}) => {
     const {name,calories, preparing_time} = cookList
   return (
-    <div className='flex justify-around items-center'>
+    <div className='flex flex-col justify-around items-center'>
+
+
+      <div className='lg:hidden md:hidden sm:hidden flex'>
+    <p>{id+1}</p>
+      </div>
+<div className='flex justify-center items-center gap-7'>
+
  <div className='grid-cols-[55px,1.6fr,1fr,1fr,2fr] lg:hidden md:hidden sm:hidden flex flex-col'>
         <p> </p>
-        <p className='text-left font-bold mb-3'>name</p>
-        <p className='text-left font-bold mb-3'>Time</p>
-        <p className='text-left font-bold mb-3'>Calories</p>
-        
+        <p className='text-left font-bold mb-2'>Name</p>
+        <p className='text-left font-bold mb-2'>Time</p>
+        <p className='text-left font-bold mb-4'>Calories</p>
+       <div className='block h-12'></div>
         </div>
+
     <div className='lg:grid md:grid sm:grid items-start grid-cols-[40px,2fr,1fr,1fr,1fr,1fr] my-4 py-2 border-b-2 border-gray-500 flex flex-col'>
-        <p className='text-right place-self-end self-center font-bold p-2'>{id+1}</p>
-        <p className='text-left'>{name}</p>
-        <p className='text-left'>{preparing_time}</p>
-        <p className='text-left'>{calories} calories</p>
-        <button><h1 className='font-bold bg-lime-400 px-4 py-2 rounded-full'>Preparing</h1></button>
+        <p className='   font-bold  lg:grid md:grid sm:grid hidden'>{id+1}</p>
+        <p className='text-left font-bold mb-2'>{name}</p>
+        <p className='text-left font-bold mb-2'>{preparing_time}</p>
+        <p className='text-left font-bold mb-4'>{calories} calories</p>
+        <button><h1 className='font-bold bg-lime-400 px-4 py-2 rounded-full lg:mt-5  '>Preparing</h1></button>
+
+
+        {/* <p className='text-left'>{name}</p>
+        <p className='text-left font-bold mb-2'>{preparing_time}</p>
+        <p >{calories} calories</p>
+         */}
     </div>
+
+</div>
     </div>
   )
 }
